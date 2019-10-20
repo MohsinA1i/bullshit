3c0ae6003e43b25a96002664a96e4380cee167bb
Mohsin Ali <mohsin.ali.the.great@gmail.com>
2019-10-20 20:59:48 +0500

    undid test changes

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ io.on("connection", function(socket) {
 	let userID = parseInt(socket.handshake.query.id);
 	if (userID != null) {
 		if (sockets.has(userID)) {
-			socket.disconnect(false);
+			socket.disconnect(true);
 			return;
 		}
 		console.log(userID + " connected | transport " + socket.conn.transport.name);
